35793212adedb7c812a1601ade4b13a09d4ddc26
Dannyyp <danielpearson@hotmail.com>
2021-01-25 17:42:47 +0000

    Add files via upload

---
+++ Module 2/Programming Assignment 2/cachematrix.R
@@ -0,0 +1,29 @@
+#Test
+makeCacheMatrix <- function(x = matrix()){
+  inverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inv) inverse <<- inv
+  getInverse <- function() inverse
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...){
+  inverse <- x$getInverse()
+  
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
+}
